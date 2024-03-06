8424bc7e624bf2aa681e7bda5844b13524bc11f0
chocs <144632964+ochocss@users.noreply.github.com>
2024-03-05 23:57:00 -0300

    Update TictactoeGame.java
    creating bot in tic tac toe

--- src/com/chocs/minigames/tictactoe/TictactoeGame.java
+++ src/com/chocs/minigames/tictactoe/TictactoeGame.java
@@ -3,8 +3,8 @@ package com.chocs.minigames.tictactoe;
 import javax.swing.JFrame;
 
 public class TictactoeGame extends JFrame {
-    public TictactoeGame() {
-        this.add(new TictactoeGraphics());
+    public TictactoeGame(boolean isLocalGame) {
+        this.add(new TictactoeGraphics(isLocalGame));
         this.pack();
         this.setTitle("Tic Tac Toe");
         this.setResizable(false);
